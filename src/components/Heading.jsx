0d87a2e2eeb0d398b8008a234ca1828ce574fff5
IPSITA_Pradhan <159911096+IpsitaPradhan1234@users.noreply.github.com>
2025-05-24 13:57:51 +0530

    update src

--- src/components/Heading.jsx
+++
@@ -1,7 +0,0 @@
-export const Heading=({label})=>{
-    return(
-        <div className="text-center text-4xl font-bold mb-1">
-            {label}
-        </div>
-    )
-}
--- src/components/Subheading.jsx
+++
@@ -1,7 +0,0 @@
-export const Subheading = ({label})=>{
-    return(
-        <div className="text-gray-500 pb-12 ">
-            {label}
-        </div>
-    )
-}
